5c1c36862c2e2d4e925772b41d4fcdd35f010942
Alula <132002275+AlulaGebremariam@users.noreply.github.com>
2023-06-22 15:58:05 -0500

    edit

--- src/pages/Portfolio.js
+++ src/pages/Portfolio.js
@@ -1,14 +1,42 @@
-import React from 'react'
-import Images from '../components/Images'
-import Nav from '../components/Nav'
-
+import React from "react";
+import Images from "../components/Images";
+import Nav from "../components/Nav";
+import {
+  img1,
+  img2,
+  img3,
+  img4,
+  img5,
+  img6,
+  img7,
+  img8,
+  img9,
+  img10,
+  img11,
+  img12,
+  img13,
+  img14,
+  img15,
+  img16,
+  img17,
+  img18,
+} from "../assets/image";
 function Portfolio() {
   return (
     <div>
-        <Nav />
-      <Images />
+      <Nav />
+      <Images title="wedding" a={img1} b={img2} c={img3} d={img4} e={img5} />
+      <Images title="model" a={img6} b={img7} c={img8} d={img9} e={img10} />
+      <Images
+        title="single"
+        a={img11}
+        b={img12}
+        c={img13}
+        d={img14}
+        e={img15}
+      />
     </div>
-  )
+  );
 }
 
-export default Portfolio
+export default Portfolio;
